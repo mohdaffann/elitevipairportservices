c72443f6c184f21ccaf7774239a6b5ffc6077bc8
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-08-03 12:19:29 +0530

    mobile responsive imageslider

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -3,20 +3,24 @@ import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 function ImageSlider() {
     const [mobile, Setmobile] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
     useEffect(() => {
         const checkInterface = () => {
             Setmobile(window.innerWidth < 768)
         }
         checkInterface();
+        setIsLoaded(true);
         window.addEventListener('resize', checkInterface)
         return () => window.removeEventListener('resize', checkInterface)
     }, [])
 
     const desktoppics = ['/LondonEye.jpg', '/LondonBridge.jpg', '/Hollywood.jpg', '/Eiffel.jpg']
-    const mobilePics = ['/LondonTower.jpg', 'LondonBridgeMobile.jpg', '/Hollywood.jpg', '/Eiffel.jpg']
+    const mobilePics = ['/LondonTower.jpg', '/LondonBridgeMobile.jpg', '/Hollywood.jpg', '/Eiffel.jpg']
     const pics = mobile ? mobilePics : desktoppics;
+
+    if (!isLoaded) return null;
     return (
-        <AwesomeSlider organicArrows={true} bullets={false} mobileTouch={true} className="h-screen" startup={false} >
+        <AwesomeSlider organicArrows={true} bullets={false} mobileTouch={true} className="h-screen" startup={true} >
             {pics.map((item, ind) => (
                 <div key={ind} data-src={item} />
             ))}
