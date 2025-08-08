bcb41c0bfe736ed37c6214332fd5383e815e6ca6
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-08-08 17:07:06 +0530

    srroll top when link clicked

---
+++ src/components/ScrollTop.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+function ScrollTop() {
+    const path = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [path]);
+    return null;
+}
+
+export default ScrollTop;
