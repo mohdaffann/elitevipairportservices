ae5fa45e13a8ce52858f2340882093381b7869c0
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-08-08 17:07:49 +0530

    scroll to top comp on top of layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from '../components/Header.jsx';
 import Footer from "../components/Footer.jsx";
+import ScrollTop from "../components/ScrollTop.jsx";
 function Layout() {
     return (
-        <div className="min-h-screen bg-gray-800 flex flex-col">
+        <div className="min-h-screen bg-gray-800 flex flex-col overflow-hidden ">
+            <ScrollTop />
             <Header />
             <Outlet />
             <Footer />
