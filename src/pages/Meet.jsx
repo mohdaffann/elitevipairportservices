106bad800d3f5c8f30fdcd36597a3bb6a5c8d336
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-08-08 17:09:39 +0530

    design change in services pages

--- src/pages/Meet.jsx
+++ src/pages/Meet.jsx
@@ -2,15 +2,34 @@ import React from "react";
 
 function Meet() {
     return (
-        <div className="w-full flex flex-col items-center px-4 md:px-8 py-10 mt-8">
-            <h1 className="text-center text-white text-4xl font-bold mb-6">MEET & GREET</h1>
-            <div className="w-full flex flex-col items-center space-y-8 max-w-6xl">
-                <img src="/Meet.jpg" className="w-full max-w-3xl rounded-lg shadow-lg object-cover" />
+        <div className="w-full px-4 py-20 flex flex-col items-center mt-3">
 
-                <p className="text-white text-lg md:text-xl leading-relaxed max-w-3xl text-center">
-                    Our Meet & Greet service ensures a smooth, personalized airport experience from the moment you arrive. Whether you're landing or taking off, our professional greeters will assist you with fast-track immigration, baggage handling, and escorting you through the airport with ease. Enjoy a hassle-free journey with priority access and dedicated support, designed for comfort, efficiency, and a touch of VIP luxury. Ideal for first-time travelers, business executives, seniors, and families, our Meet & Greet service is the perfect start or end to your trip.
-                </p>
+            <div className="w-full max-w-6xl mb-10">
+                <div className="inline-block bg-gray-600 text-white px-6 py-3 text-xl font-bold tracking-wide shadow-md">
+                    MEET & GREET
+                </div>
+            </div>
 
+            <div className="w-full max-w-6xl flex flex-col md:flex-row md:space-x-8 items-start">
+
+                <img
+                    src="/Meet.jpg"
+                    alt="Chauffeur"
+                    className="w-[400px]  rounded shadow-lg mb-6 md:mb-0 object-cover"
+                />
+
+                <div className="text-white  text-base md:text-lg leading-relaxed md:w-1/2 space-y-4 ">
+                    <p>
+                        Our Meet & Greet service ensures a smooth, personalized airport experience from the moment you arrive.
+                    </p>
+                    <p>
+                        Whether you're landing or taking off, our professional greeters will assist you with fast-track immigration, baggage handling, and escorting you through the airport with ease.
+                    </p>
+                    <p>
+                        Enjoy a hassle-free journey with priority access and dedicated support, designed for comfort, efficiency, and a touch of VIP luxury.
+                    </p>
+
+                </div>
             </div>
         </div>
     )
